4f7c5dda53bee09df9a4c0d2ba60e12a8be34b6b
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-26 22:57:51 -0300

    Implements function in Express

--- api/controllers/fib.js
+++ api/controllers/fib.js
@@ -1,7 +1,18 @@
 module.exports = function (app) {
-	app.get("/fib/{}", function (oRequest, oResponse) {
-		var oRepository = new app.api.repository.fibRepository();
-		
+	var oMemoize = new app.api.util.memoize();
+	var oRepository = new app.api.repository.fibRepository();
+
+	app.param("id", function (oRequest, oResponse, fnNext, iValue) {
+		if (!Number.isInteger(iValue)) {
+			oResponse.status(400).json("Error value");
+		}
+		oRequest.iValue = iValue;
+		fnNext();
+	});
+	
+	app.get("/fib/:id", function (oRequest, oResponse) {
+		var iValue = oRequest.iValue;
+		var sResult = oMemoize.call(oRepository.fibonacci, iValue);
 		oResponse.status(200).json("success");
 	});
 }
